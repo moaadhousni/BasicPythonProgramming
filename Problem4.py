7662dc2279cef500b46141e26150c6e4a31ad993
moaadhousni <116218658+moaadhousni@users.noreply.github.com>
2023-02-14 23:18:39 -0500

    problem4
    Problem 4:  Order check. Compose a program that accepts three floats x, y, and z as command-line arguments and writes True if the values are strictly ascending or descending (x < y < z or x > y > z), and False otherwise.

---
+++ Problem4.py
@@ -0,0 +1,11 @@
+#Problem 4
+
+x =   float(input("enter a number"))
+y =   float(input("enter a number"))
+z =   float(input("enter a number"))
+if  (x<y)and(x<z):
+    print("True")
+elif (x>y)and(x>z):
+    print("True")
+else:
+    print("False")
